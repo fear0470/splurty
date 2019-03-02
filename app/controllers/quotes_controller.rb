0bf7047db30f0d6eca56bbfcc7929acbb5875eb5
Nicholas Fearns <you@example.com>
2019-03-02 17:45:41 +0000

    completed tutorial on builing the about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,9 +9,15 @@ class QuotesController < ApplicationController
   end  
 
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
     redirect_to root_path
   end  
+
+  def about
+  end
 
 
   private
